328cc09fc607ec157882561f84ddf5927988b943
Otega Otto <otegaotto@Otegas-MBP.lan>
2023-06-17 00:44:26 -0500

    forget-password firebase integration || #36 login and authentication complete

--- fiverr/lib/ForgetPassword/body.dart
+++ fiverr/lib/ForgetPassword/body.dart
@@ -1,4 +1,7 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:fiverr/DialogBox/error_dialog.dart';
 import 'package:fiverr/ForgetPassword/background.dart';
+import 'package:fiverr/LoginScreen/login_screen.dart';
 import 'package:flutter/material.dart';
 
 class ForgetBody extends StatefulWidget {
@@ -9,7 +12,26 @@ class ForgetBody extends StatefulWidget {
 
 class _ForgetBodyState extends State<ForgetBody> {
 
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
   final TextEditingController _forgetPassTextController = TextEditingController(text: '');
+
+  void _forgetPassSubmitForm() async
+  {
+    try
+    {
+      await _auth.sendPasswordResetEmail(
+        email: _forgetPassTextController.text,
+      );
+      // ignore: use_build_context_synchronously
+      Navigator.pushReplacement(context,
+        MaterialPageRoute(builder: (context) => LoginScreen()));
+    }
+    catch(error)
+    {
+      ErrorAlertDialog(message: error.toString(),);
+    }
+  }
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
@@ -17,11 +39,11 @@ class _ForgetBodyState extends State<ForgetBody> {
       child: Stack(
         children: [
           Padding(
-            padding: EdgeInsets.symmetric(horizontal: 40, vertical: 40),
+            padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 40),
             child: ListView(
               children: [
                 SizedBox(height: size.height * 0.2,),
-                Text(
+                const Text(
                   'Forget Password',
                   style: TextStyle(
                     color: Colors.black,
@@ -31,7 +53,7 @@ class _ForgetBodyState extends State<ForgetBody> {
                   ),
                 ),
                 const SizedBox(height: 10,),
-                Text(
+                const Text(
                   'Email address',
                   style: TextStyle(
                     color: Colors.black,
@@ -43,12 +65,38 @@ class _ForgetBodyState extends State<ForgetBody> {
                 const SizedBox(height: 20,),
                 TextField(
                   controller: _forgetPassTextController,
-                  decoration: InputDecoration(
+                  decoration: const InputDecoration(
                     filled: true,
                     fillColor: Colors.black38,
                     enabledBorder: UnderlineInputBorder(
                       borderSide: BorderSide(color: Colors.white),
                     ),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 20,),
+                MaterialButton(
+                  onPressed: ()
+                  {
+                    _forgetPassSubmitForm();
+                  },
+                  color: Colors.black54,
+                  elevation: 20,
+                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: const Padding(
+                    padding: EdgeInsets.symmetric(vertical: 14),
+                    child: Text(
+                      'Reset now',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontStyle: FontStyle.italic,
+                        fontSize: 20,
+                      ),
+                    ), 
                   ),
                 ),
               ],
